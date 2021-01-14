f843558220e9a390306ed0e7ba335b6335142ec8
Vivek Rai <mail@raivivek.in>
2021-01-14 16:42:33 -0500

    change to heroku app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python rss2cal.py $PORT
--- rss2cal.py
+++ rss2cal.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python
 
-from ics import Calendar, Event
-import feedparser
-import arrow
-import sys
+import os
+import tempfile
 
-c = Calendar(creator="rss2cal.py by raivivek")
+import arrow
+import feedparser
+from flask import Flask, render_template, send_file, request
+from ics import Calendar, Event
+
+
+app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 
 def create_calendar(uri):
+    c = Calendar(creator="rss2cal.py by raivivek")
     parsed_feed = feedparser.parse(uri)
     for entry in parsed_feed.entries:
         e = Event()
@@ -20,7 +26,21 @@ def create_calendar(uri):
         e.categories = [entry.category]
 
         c.events.add(e)
+    return c
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    with tempfile.NamedTemporaryFile(mode='w') as f:
+        f.writelines(create_calendar(request.form['url']))
+
+        return send_file(f.name, mimetype="text/calendar", as_attachment=True)
+
 
 if __name__=="__main__":
-    create_calendar(sys.argv[1])
-    print(c, file=sys.stdout)
+    app.run(host='0.0.0.0', port=port, debug=False)
